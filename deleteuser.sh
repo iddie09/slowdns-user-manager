9903e930fd23e28aefa7182b38cc0f21ce4231cf
iddie09 <iddie.nine.15@gmail.com>
2026-03-09 09:58:02 +0300

    Update deleteuser.sh

--- deleteuser.sh
+++ deleteuser.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+clear
+
+echo "══════ DELETE USER ══════"
+echo ""
+
+read -p "Enter username: " user
+
+userdel -r $user
+
+iptables -F $user
+iptables -X $user
+
+sed -i "/^$user /d" /etc/slowdns-manager/users.db
+
+echo ""
+echo "User deleted successfully"
+
+read -p "Press Enter"
+menu
