7d97cc0f885e1940438ba5474d16d5ee0916cc9e
iddie09 <iddie.nine.15@gmail.com>
2026-03-09 09:57:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+apt update -y
+apt install iptables cron bc -y
+
+mkdir -p /etc/slowdns-manager
+
+cp *.sh /etc/slowdns-manager/
+
+cp menu /usr/local/bin/menu
+chmod +x /usr/local/bin/menu
+
+chmod +x /etc/slowdns-manager/*.sh
+
+touch /etc/slowdns-manager/users.db
+
+echo "*/1 * * * * root bash /etc/slowdns-manager/monitor.sh" >> /etc/crontab
+
+echo "Installation Complete"
+echo "Run: menu"
